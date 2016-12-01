4f6ac956f7596f19caecb2cc1df81a75ec4bbb91
SergoRz <sanchez13rz@gmail.com>
2016-12-01 17:24:40 +0100

    Borrado clases A y B

--- app/src/main/java/com/example/versus/tictactoe/B.java
+++
@@ -1,8 +0,0 @@
-package com.example.versus.tictactoe;
-
-/**
- * Created by Versus on 01/12/2016.
- */
-
-public class B {
-}
--- app/src/main/java/com/example/versus/tictactoe/ClaseA.java
+++
@@ -1,9 +0,0 @@
-package com.example.versus.tictactoe;
-
-/**
- * Created by EmilioCB on 01/12/2016.
- */
-
-public class ClaseA {
-    //Esta es la clase A
-}
