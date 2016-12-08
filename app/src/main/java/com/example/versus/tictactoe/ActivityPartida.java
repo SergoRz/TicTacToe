184f5108b08dfcaaeb65f75126625add3001b743
SergoRz <sanchez13rz@gmail.com>
2016-12-08 18:55:34 +0100

    InicioPartida

--- app/src/main/java/com/example/versus/tictactoe/ActivityPartida.java
+++ app/src/main/java/com/example/versus/tictactoe/ActivityPartida.java
@@ -9,12 +9,9 @@ public class ActivityPartida extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partida);
-        Partida p;
-        Jugador j1;
-        Jugador j2;
-
-
-
+        Jugador j1 = null;
+        Jugador j2 = null;
+        Partida p = new Partida(j1,j2);
     }
 
     public void colocarPieza(){
--- app/src/main/java/com/example/versus/tictactoe/Partida.java
+++ app/src/main/java/com/example/versus/tictactoe/Partida.java
@@ -6,11 +6,12 @@ public class Partida{
 
     private Jugador j1;
     private Jugador j2;
-    private ArrayList<Combinacion> combinacinesGanadoras = new ArrayList<Combinacion> ();
+    private ArrayList<Combinacion> combinacionesGanadoras = new ArrayList<Combinacion> ();
 
     public Partida(Jugador j1, Jugador j2) {
         this.j1 = j1;
         this.j2 = j2;
+        iniciarPartida();
     }
 
     public void iniciarPartida(){
@@ -29,9 +30,61 @@ public class Partida{
 
         //Creacion de las combinaciones ganadoras
         ArrayList<Pieza> aP1 = new ArrayList<Pieza>();
+        aP1.add(p11);
+        aP1.add(p12);
+        aP1.add(p13);
+        Combinacion cGanadora1 = new Combinacion(aP1);
 
-        //Combinacion cGanadora1 = new Combinacion;
+        ArrayList<Pieza> aP2 = new ArrayList<Pieza>();
+        aP2.add(p21);
+        aP2.add(p22);
+        aP2.add(p23);
+        Combinacion cGanadora2 = new Combinacion(aP2);
 
+        ArrayList<Pieza> aP3 = new ArrayList<Pieza>();
+        aP3.add(p31);
+        aP3.add(p32);
+        aP3.add(p33);
+        Combinacion cGanadora3 = new Combinacion(aP3);
+
+        ArrayList<Pieza> aP4 = new ArrayList<Pieza>();
+        aP4.add(p11);
+        aP4.add(p21);
+        aP4.add(p31);
+        Combinacion cGanadora4 = new Combinacion(aP4);
+
+        ArrayList<Pieza> aP5 = new ArrayList<Pieza>();
+        aP5.add(p12);
+        aP5.add(p22);
+        aP5.add(p32);
+        Combinacion cGanadora5 = new Combinacion(aP5);
+
+        ArrayList<Pieza> aP6 = new ArrayList<Pieza>();
+        aP6.add(p13);
+        aP6.add(p23);
+        aP6.add(p33);
+        Combinacion cGanadora6 = new Combinacion(aP6);
+
+        ArrayList<Pieza> aP7 = new ArrayList<Pieza>();
+        aP7.add(p11);
+        aP7.add(p22);
+        aP7.add(p33);
+        Combinacion cGanadora7 = new Combinacion(aP7);
+
+        ArrayList<Pieza> aP8 = new ArrayList<Pieza>();
+        aP8.add(p31);
+        aP8.add(p22);
+        aP8.add(p13);
+        Combinacion cGanadora8 = new Combinacion(aP8);
+
+        combinacionesGanadoras.add(cGanadora1);
+        combinacionesGanadoras.add(cGanadora2);
+        combinacionesGanadoras.add(cGanadora3);
+        combinacionesGanadoras.add(cGanadora4);
+        combinacionesGanadoras.add(cGanadora5);
+        combinacionesGanadoras.add(cGanadora6);
+        combinacionesGanadoras.add(cGanadora7);
+        combinacionesGanadoras.add(cGanadora8);
     }
 
     public boolean comprobarSolucion(){
