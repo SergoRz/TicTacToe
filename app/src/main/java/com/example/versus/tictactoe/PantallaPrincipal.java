c97bb2e7be6e485f4669e33b8e46258f5977eda4
ecabab <ecabab@gmail.com>
2016-12-08 18:07:46 +0100

    InicioConf

--- app/src/main/java/com/example/versus/tictactoe/PantallaPrincipal.java
+++ app/src/main/java/com/example/versus/tictactoe/PantallaPrincipal.java
@@ -1,14 +1,21 @@
 package com.example.versus.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class PantallaPrincipal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_principal);
+    }
+
+    public void goConfig(View v){
+        Intent intent = new Intent(PantallaPrincipal.this, Configuracion.class);
+        startActivity(intent);
     }
 }
