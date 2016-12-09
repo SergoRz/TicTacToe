9d7c62bb990a69c84fc1873510c67270bdaf6575
SergoRz <sanchez13rz@gmail.com>
2016-12-09 14:23:12 +0100

    Funcionamento del juego sin comprobar ganador

--- app/src/main/java/com/example/versus/tictactoe/ActivityPartida.java
+++ app/src/main/java/com/example/versus/tictactoe/ActivityPartida.java
@@ -1,11 +1,9 @@
 package com.example.versus.tictactoe;
 
-import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
